671a7550dd17be0b49470a67d1340d668ce5475a
Deiver David <resdeiver50@gmail.com>
2022-02-01 22:10:39 -0500

    se crea los datos falsos necesarios para la tabla de proveedores

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -14,7 +14,18 @@ class ProviderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'nit' => rand(1,100),
+            'cedula' => rand(1, 1000),
+            'business_name' => $this->faker->company(),
+            'property_name' => $this->faker->sentence(),
+            'name' => $this->faker->name(),
+            'lastname1' => $this->faker->lastname(),
+            'lastname2' => $this->faker->lastname(),
+            'address' => $this->faker->address(),
+            'phone_number1' => $this->faker->phoneNumber(),
+            'phone_number2' => $this->faker->phoneNumber(),
+            'fax' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email(),
         ];
     }
 }
