cc14f2d051374a130bf10a98923c7b193b36d111
Deiver David <resdeiver50@gmail.com>
2022-02-05 01:11:55 -0500

    se crea el array para que se pueda modificar masivamente

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Provider extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'nit',
+        'cedula',
+        'business_name',
+        'property_name',
+        'name',
+        'lastname1',
+        'lastname2',
+        'address',
+        'phone_number1',
+        'phone_number2',
+        'fax',
+        'email',
+    ];
 }
